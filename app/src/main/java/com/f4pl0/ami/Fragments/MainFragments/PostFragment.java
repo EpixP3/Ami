6ff76f0e24f49e0738c23a86102a40dbdedd1d6d
epixp3 <straka.filip@gmail.com>
2018-11-30 00:10:11 +0100

    Added floating action button for adding posts, has bugs

--- app/src/main/java/com/f4pl0/ami/Fragments/MainFragments/PostFragment.java
+++ app/src/main/java/com/f4pl0/ami/Fragments/MainFragments/PostFragment.java
@@ -8,12 +8,14 @@ import android.view.ViewGroup;
 
 import com.f4pl0.ami.R;
 
-
 public class PostFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post, container, false);
+        View fragmentView = inflater.inflate(R.layout.fragment_post, container, false);
+
+        return fragmentView;
     }
 }
